5194218fdbbf014550cb0c8930b81035fcde3d3a
Pete Davis <pete.davis@rcn.org.uk>
2017-06-01 15:42:56 +0100

    Basic typography and layout

--- header.php
+++ header.php
@@ -15,7 +15,7 @@
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
-
+<link href="https://fonts.googleapis.com/css?family=Roboto+Slab|Roboto:900" rel="stylesheet">
 <?php wp_head(); ?>
 </head>
 
